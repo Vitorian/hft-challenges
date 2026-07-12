98506026ee3cacacc5c11b2635e1e1f85c44ae95
Henrique Bucher <hbucher@gmail.com>
2026-07-12 12:51:56 -0500

    Challenge 11: remove unused run_workload from public benchmark
    Orphaned helper that duplicated the timed loop inlined in BM_Solution;
    never called, no effect on build or scoring. Reported on the forum by
    Claudio Raimondi (thread #32).

--- challenge-11-rolling-counter/benchmark.cpp
+++ challenge-11-rolling-counter/benchmark.cpp
@@ -4,29 +4,6 @@
 #include "common/benchmark_harness.h"
 #include "solution/solution.h"
 
-namespace {
-
-// Simulate trading: update time, add events, query count
-// Each "op" is one update + addEvent + count cycle
-void run_workload(size_t ops, int64_t interval_ns, int64_t precision_ns,
-                  int64_t time_step_ns, size_t events_per_step, uint64_t seed) {
-    hftu::RollingCounter rc(interval_ns, precision_ns);
-    std::mt19937_64 gen(seed);
-    std::uniform_int_distribution<size_t> event_dist(0, events_per_step * 2);
-    std::uniform_int_distribution<int64_t> jitter_dist(0, time_step_ns / 2);
-
-    int64_t t = 0;
-    for (size_t i = 0; i < ops; ++i) {
-        t += time_step_ns + jitter_dist(gen);
-        rc.update(t);
-        rc.addEvent(event_dist(gen));
-        size_t c = rc.count();
-        hftu::do_not_optimize(c);
-    }
-}
-
-} // namespace
-
 // Standard: 1s window, 1ms precision, ~1000 events/sec
 static hftu::RegisterBenchmark reg_solution(
     "BM_Solution", 1'000'000,
